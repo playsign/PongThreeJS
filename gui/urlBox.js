8c563fca577f2832205b3d2c66cb5df9c8527621
Tapani <tapani.jamsa@gmail.com>
2013-09-20 09:52:53 +0300

    New multiplayer URL box

--- gui/urlBox.js
+++ gui/urlBox.js
@@ -9,19 +9,4 @@ $(function()
 		show: { effect: 'fade', duration: 500 },
 		hide: { effect: 'fade', duration: 500 } 
 	});
-	
-	//  $("#infoButton")
- //       .text("") // sets text to empty
-	// .css(
-	// { "z-index":"2",
-	//    "background-image": "url('images/smallButton.png')",
-	//   "position":"absolute", "top":"4px", "left":"4px"
-	// }) // adds CSS
- //    .append("<img width='48' height='44' src='images/infoIcon.png'/>")
- //    .button()
-	// .click( 
-	// 	function() 
-	// 	{ 
-	// 		$("#infoBox").dialog("open");
-	// 	});
 });
--- p2p.js
+++ p2p.js
@@ -1,10 +1,10 @@
 var peerJsApiKey = "gnyz9wskc2chaor";
 
 function getPeerIdFromURL() {
-    if(window.location.search) {
+    if (window.location.search) {
         var params = window.location.search.substring(1).split('&');
-        for(var i = 0; i < params.length; ++ i) {
-            if(params[i].match('^peer-id')) {
+        for (var i = 0; i < params.length; ++i) {
+            if (params[i].match('^peer-id')) {
                 return params[i].split('=')[1];
             }
         }
@@ -31,31 +31,37 @@ function Vec3FromArray(a) {
     var v3 = new THREE.Vector3();
     v3.copy(a);
     return v3;
- }
+}
+
 function gotData(data) {
-        try {
-            msg = JSON.parse(data);
-        } catch (err) {
-            console.log("not JSON: " + data);
-            return;
+    try {
+        msg = JSON.parse(data);
+    } catch (err) {
+        console.log("not JSON: " + data);
+        return;
+    }
+    if (netRole === 'client' && msg.ballpos !== undefined) {
+        msg.ballpos = Vec3FromArray(msg.ballpos);
+        msg.racketspos = msg.racketspos.map(function(arr) {
+            return Vec3FromArray(arr);
+        });
+        if (clientUpdateCallback) {
+            var keys = clientUpdateCallback(msg);
+            peerConnection.send(JSON.stringify({
+                pressedkeys: keys
+            }))
+        } else {
+            console.log("update msg without handler, in " + netRole + " mod");
         }
-        if (netRole === 'client' && msg.ballpos !== undefined) {
-            msg.ballpos = Vec3FromArray(msg.ballpos);
-            msg.racketspos = msg.racketspos.map(
-                function(arr) { return Vec3FromArray(arr); });
-            if (clientUpdateCallback) {
-                var keys = clientUpdateCallback(msg);
-                peerConnection.send(JSON.stringify({pressedkeys: keys}))
-            } else {
-                console.log("update msg without handler, in " + netRole + " mod");
-            }
-        } else if (netRole === 'server' && msg.pressedkeys !== undefined) {
-                clientKeysPressed = msg.pressedkeys;
-        }
+    } else if (netRole === 'server' && msg.pressedkeys !== undefined) {
+        clientKeysPressed = msg.pressedkeys;
+    }
 }
 
 function initNet(updateCallback) {
-    var pjs = new Peer({key: peerJsApiKey})
+    var pjs = new Peer({
+        key: peerJsApiKey
+    })
     var peerid = getPeerIdFromURL();
 
     if (peerid !== null) {
@@ -70,16 +76,18 @@ function initNet(updateCallback) {
     } else {
         netRole = 'server';
     }
-    
+
     pjs.on('open', function(myid) {
         if (peerid === null) {
-            var gamemsg = "game url: " + window.location.href + '?peer-id=' + myid
-            alert(gamemsg);
+            var gamemsg = window.location.href + '?peer-id=' + myid
+            // alert(gamemsg);
+            $("#playerUrl").html(gamemsg);
+            $("#urlBox").dialog("open");
             console.log(gamemsg);
         }
     });
 
-    pjs.on('connection', function(conn){
+    pjs.on('connection', function(conn) {
         gotConnection(conn);
     });
 }
@@ -97,6 +105,6 @@ function serverNetUpdate(racketPositions, ballPos, timedelta) {
         } else {
             console.log("no peer");
         }
-            
+
     }
 }
